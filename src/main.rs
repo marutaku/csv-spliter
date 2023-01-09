b18511456070cae3087c28d434ac9a7ec5034e31
Takumi Maruyama <satouredstone@gmail.com>
2023-01-09 22:46:04 +0900

    ファイル構造変更

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,28 @@
 use std::env;
-use std::process;
 
-use crate::split::CSVFragment;
+use clap::Parser;
 
+mod fragment;
 mod split;
 
-fn generate_csv_file_name(file_prefix: &str, index: usize) -> String {
-    format!("{}{}.csv", file_prefix, index)
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    input_filename: String,
+    line_count: usize,
+    #[arg(short, long)]
+    prefix: Option<String>,
+    #[arg(short, long)]
+    output_dir: Option<String>,
 }
 
 fn main() {
     let filepath: String = env::args().nth(1).expect("File path not given");
     let line_count_str: String = env::args().nth(2).expect("Missing line count");
-    let file_prefix = "prefix_"; // TODO: 引数として受け取る
-
     let line_count: usize = line_count_str
         .parse()
         .expect("Line count should be a number");
-    let mut reader = match csv::Reader::from_path(&filepath) {
-        Err(why) => {
-            println!("cannot open {}: {}", &filepath, why);
-            process::exit(1);
-        }
-        Ok(content) => content,
-    };
-    let header = reader.headers().expect("No header").clone();
-    let mut csv_flagment = CSVFragment::new(header.clone(), line_count);
-    let mut file_iteration_counter = 1;
-    for result in reader.records() {
-        let record = match result {
-            Err(error) => {
-                println!("wrong csv format {}", error);
-                process::exit(1)
-            }
-            Ok(record) => record,
-        };
-        csv_flagment.push(record);
-        if csv_flagment.is_full() {
-            let csv_file_name = generate_csv_file_name(file_prefix, file_iteration_counter);
-            csv_flagment
-                .write_records_to_file(csv_file_name.as_str())
-                .expect("cannnot create file");
-            file_iteration_counter += 1;
-            csv_flagment = CSVFragment::new(header.clone(), line_count);
-        }
-    }
-    if csv_flagment.len() != 0 {
-        csv_flagment
-            .write_records_to_file(
-                generate_csv_file_name(file_prefix, file_iteration_counter).as_str(),
-            )
-            .expect("can not create file");
-    }
+    let file_prefix = "prefix_"; // TODO: 引数として受け取る
+
+    split::split_csv(filepath, line_count, file_prefix).expect("Split csv failed: ");
 }
--- src/split.rs
+++ src/split.rs
@@ -1,39 +1,51 @@
-use csv::{StringRecord, Writer};
 use std::error::Error;
+use std::process;
 
-pub struct CSVFragment {
-    header: StringRecord,
-    records: Vec<StringRecord>,
-    capacity: usize,
+use crate::fragment;
+
+fn generate_csv_file_name(file_prefix: &str, index: usize) -> String {
+    format!("{}{}.csv", file_prefix, index)
 }
 
-impl CSVFragment {
-    pub fn new(header: StringRecord, capacity: usize) -> Self {
-        CSVFragment {
-            header: header.clone(),
-            records: Vec::<StringRecord>::with_capacity(capacity),
-            capacity,
+pub fn split_csv(
+    filepath: String,
+    line_count: usize,
+    file_prefix: &str,
+) -> Result<(), Box<dyn Error>> {
+    let mut reader = match csv::Reader::from_path(&filepath) {
+        Err(why) => {
+            println!("cannot open {}: {}", &filepath, why);
+            process::exit(1);
+        }
+        Ok(content) => content,
+    };
+    let header = reader.headers().expect("No header").clone();
+    let mut csv_flagment = fragment::CSVFragment::new(header.clone(), line_count);
+    let mut file_iteration_counter = 1;
+    for result in reader.records() {
+        let record = match result {
+            Err(error) => {
+                println!("wrong csv format {}", error);
+                process::exit(1)
+            }
+            Ok(record) => record,
+        };
+        csv_flagment.push(record);
+        if csv_flagment.is_full() {
+            let csv_file_name = generate_csv_file_name(file_prefix, file_iteration_counter);
+            csv_flagment
+                .write_records_to_file(csv_file_name.as_str())
+                .expect("cannnot create file");
+            file_iteration_counter += 1;
+            csv_flagment = fragment::CSVFragment::new(header.clone(), line_count);
         }
     }
-
-    pub fn write_records_to_file(&self, filepath: &str) -> Result<(), Box<dyn Error>> {
-        let mut writer = Writer::from_path(filepath)?;
-        writer.write_record(&self.header)?;
-        for record in &self.records {
-            writer.write_record(record)?;
-        }
-        writer.flush()?;
-        Ok(())
+    if csv_flagment.len() != 0 {
+        csv_flagment
+            .write_records_to_file(
+                generate_csv_file_name(file_prefix, file_iteration_counter).as_str(),
+            )
+            .expect("can not create file");
     }
-
-    pub fn push(&mut self, row: StringRecord) {
-        self.records.push(row);
-    }
-
-    pub fn is_full(&self) -> bool {
-        self.records.len() == self.capacity
-    }
-    pub fn len(&self) -> usize {
-        self.records.len()
-    }
+    Ok(())
 }
